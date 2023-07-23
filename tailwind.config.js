44504398dae7f7d28929e8a903959db78d93c824
Sandis <sandissirmais8@gmail.com>
2023-07-23 14:29:32 +0300

    magebit commit from local IDE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      width: {
-        '680': '680px',
-        '520': '520px',
-      
-
-      },
       maxWidth: {
         '680': '680px',
         '520': '520px',
         '480': '480px',
-      }
+      },
+      colors: {
+        'darkblue': '#233759',
+        'lightblue': '#4066A5',
+        'darkgrey': '#6A707B',
+      },
+      boxShadow: {
+        '3xl': '0px 20px 40px 0px #1318214D',
+      },
     },
   },
   plugins: [],
+  safelist: [
+    'shadow-3xl',
+    {
+      pattern:
+        /(bg|text|border)-(darkblue|lightblue|darkgrey)/,
+    },
+  ],
 }
-
